10746c366d608cc1a1cb14c5941852655b01f62d
heluansantos <heluan.caetano@gmail.com>
2023-05-30 17:29:29 -0300

    feat: Course card component

---
+++ src/components/CourseCard/index.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+// Assets
+import Card from "../../assets/imgs/card.png";
+import IconDarkCalendar from "../../assets/imgs/icon-dark-calendar.png";
+import IconDarkClock from "../../assets/imgs/icon-dark-clock.png";
+import IconArrowRight from "../../assets/imgs/icon-arrow-right-opacity.png";
+
+const CourseCard: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <img src={Card} className="m-2 absolute" alt="card" />
+
+      <div className="bg-[#FFCE59] rounded-t-[20px] p-4 py-6 absolute mt-[85px] mr-[195px]">
+        <span className="flex not-italic font-normal text-sm leading-4 text-black -mt-4">
+          Academia
+        </span>
+      </div>
+      <div className="w-[285px] h-48 flex-col absolute bg-white rounded-[20px] mt-72 shadow-xl p-4">
+        <div className="flex items-center">
+          <span className="not-italic font-semibold text-base leading-5 text-black">
+            Treino de Bíceps e Tríceps
+          </span>
+        </div>
+
+        <div>
+          <span className="flex py-4 not-italic font-normal text-sm leading-4 text-black">
+            <img
+              src={IconDarkCalendar}
+              className="mr-2"
+              alt="icon-dark-calendar"
+            />
+            De 26 a 28 de Maio
+          </span>
+        </div>
+
+        <div>
+          <span className="flex not-italic font-normal text-sm leading-4 text-black">
+            <img src={IconDarkClock} className="mr-2" alt="icon-dark-clock" />
+            08 Horas
+          </span>
+        </div>
+
+        <div className="flex mt-6 justify-between">
+          <a
+            href="www"
+            className="mr-2 rounded-[200px] bg-orange-600 p-2 items-center px-3"
+          >
+            <span className="not-italic font-bold text-sm leading-4 text-white mr-4">
+              Comprar
+            </span>
+            <span className="not-italic font-bold text-sm leading-4 text-white">
+              15€
+            </span>
+          </a>
+
+          <a
+            href="www"
+            className="flex mr-2 rounded-[200px] p-2 items-center justify-center"
+          >
+            <span className="not-italic font-bold text-sm leading-4 text-black opacity-30">
+              Saiba mais
+            </span>
+            <img
+              src={IconArrowRight}
+              className="ml-2 h-2"
+              alt="icon-arrow-right"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CourseCard;
