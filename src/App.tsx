cb8cd4b17637c533f86e2350fe731b3e4a83db4e
dannpl <dancardosobr@gmail.com>
2023-06-12 17:17:38 +0100

    feat: adjust path

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Home from "./screens/Home";
 import AppProvider from "./context";
-import "utils/firebase";
+import "./utils/firebase";
 
 const App: React.FC = () => {
   return (
