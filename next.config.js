9428cf1362bc37f6f616692d7d0922589ca4b617
dannpl <dancardosobr@gmail.com>
2023-06-13 23:56:07 +0100

    feat: remove firebase hosting

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = {
-  images: {
-    domains: ["s3.us-west-2.amazonaws.com"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "assets.example.com",
-        port: "",
-        pathname: "/account123/**",
-      },
-    ],
-  },
-};
+module.exports = {};
