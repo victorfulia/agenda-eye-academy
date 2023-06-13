6e9a15fa9e4546f78e4b51123a132d009d39a5e1
dannpl <dancardosobr@gmail.com>
2023-06-14 00:02:48 +0100

    feat: add image domains

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = {};
+module.exports = {
+  images: {
+    domains: ["s3.us-west-2.amazonaws.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.example.com",
+        port: "",
+        pathname: "/account123/**",
+      },
+    ],
+  },
+};
