73bffb4b7aee971bbe15408028d517b37893bcef
heluansantos <heluan.caetano@gmail.com>
2024-02-20 20:42:54 +0000

    feat: Fiz bug link blocked

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {};
 
 module.exports = {
   images: {
-    domains: ["s3.us-west-2.amazonaws.com"],
+    domains: [
+      "s3.us-west-2.amazonaws.com",
+      "prod-files-secure.s3.us-west-2.amazonaws.com",
+    ],
     remotePatterns: [
       {
         protocol: "https",
